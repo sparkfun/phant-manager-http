542dc6ee429a5b655e20a0cf8bc3ccaa05d47874
Todd Treece <toddtreece@gmail.com>
2014-04-18 09:58:11 -0400

    metadata and keychain are no longer passed to routes

--- routes/stream.js
+++ routes/stream.js
@@ -2,107 +2,143 @@ exports.make = function(req, res) {
   res.render('streams/make', { title: 'new stream' });
 };
 
-exports.list = function(keychain, metadata) {
+exports.list = function(req, res, next) {
 
-  return function(req, res) {
+  var self = this;
 
-    metadata.list(function(err, streams) {
+  this.metadata.list(function(err, streams) {
 
-      if(err) {
-        req.flash('danger', 'loading the stream list failed.');
-        res.redirect('/');
-        return;
-      }
-
-      streams = streams.map(function(stream) {
-        stream.publicKey = keychain.publicKey(stream.id);
-        return stream;
-      });
-
-      res.render('streams/list', {
-        title: 'public streams',
-        streams: streams
-      });
-
-    });
-
-  };
-
-};
-
-exports.view = function(keychain, metadata) {
-
-  return function(req, res) {
-
-    var id = keychain.getIdFromPublicKey(req.param('publicKey'));
-
-    metadata.get(id, function(err, stream) {
-
-      if(err) {
-        req.flash('danger', err);
-        res.redirect('/');
-        return;
-      }
-
-      res.render('streams/view', {
-        title: 'stream ' + req.param('publicKey'),
-        publicKey: req.param('publicKey'),
-        stream: stream
-      });
-
-    });
-
-  };
-
-};
-
-exports.create = function(keychain, metadata) {
-
-  return function(req, res) {
-
-    var blacklist = ['_id', 'private_key'],
-        fields = [],
-        tags = [];
-
-    if(req.param('tags').trim())
-      tags = req.param('tags').trim().split(', ');
-
-    if(req.param('fields').trim())
-      fields = req.param('fields').trim().split(', ');
-
-    for(var i=0; i < fields.length; i++) {
-
-      if(blacklist.indexOf(fields[i]) !== -1) {
-        req.flash('danger', 'sorry, but ' + fields[i] + 'is an invalid field name.');
-        res.render('streams/make', { title: 'new stream' });
-      }
-
+    if(err) {
+      err = new Error('loading the stream list failed.');
+      err.status = 500;
+      next(err);
+      return;
     }
 
-    metadata.create({
-      title: req.param('title'),
-      description: req.param('description'),
-      fields: fields,
-      tags: tags
-    }, function(err, stream) {
-
-      if (err) {
-        req.flash('danger', 'creating  stream failed: ' + err);
-        res.redirect('streams/make');
-        return;
-      }
-
-      res.render('streams/create', {
-        title: 'stream ' + keychain.publicKey(stream.id),
-        stream: stream,
-        publicKey: keychain.publicKey(stream.id),
-        privateKey: keychain.privateKey(stream.id)
-      });
-
+    streams = streams.map(function(stream) {
+      stream.publicKey = self.keychain.publicKey(stream.id);
+      return stream;
     });
 
-  };
+    res.render('streams/list', {
+      title: 'public streams',
+      streams: streams
+    });
+
+  });
 
 };
 
+exports.view = function(req, res, next) {
+
+  var id = this.keychain.getIdFromPublicKey(req.param('publicKey'));
+
+  this.metadata.get(id, function(err, stream) {
+
+    if(err) {
+      err = new Error('loading the stream failed.');
+      err.status = 500;
+      next(err);
+      return;
+    }
+
+    res.render('streams/view', {
+      title: 'stream ' + req.param('publicKey'),
+      publicKey: req.param('publicKey'),
+      stream: stream
+    });
+
+  });
+
+};
+
+exports.create = function(req, res, next) {
+
+  var fields = [],
+      tags = [],
+      self = this;
+
+  if(req.param('tags').trim()) {
+    tags = req.param('tags').split(',').map(function(tag) {
+      return tag.trim();
+    });
+  }
+
+  if(req.param('fields').trim()) {
+    fields = req.param('fields').split(',').map(function(field) {
+      return field.trim();
+    });
+  }
+
+  this.metadata.create({
+    title: req.param('title'),
+    description: req.param('description'),
+    fields: fields,
+    tags: tags
+  }, function(err, stream) {
+
+    if(err) {
+      err = new Error('creating stream failed: ' + err);
+      next(err);
+      return;
+    }
+
+    res.render('streams/create', {
+      title: 'stream ' + self.keychain.publicKey(stream.id),
+      stream: stream,
+      publicKey: self.keychain.publicKey(stream.id),
+      privateKey: self.keychain.privateKey(stream.id)
+    });
+
+  });
+
+};
+
+
+exports.remove = function(req, res, next) {
+
+  var pub = req.param('public'),
+      prv = req.param('private_key'),
+      id, err;
+
+  // check for public key
+  if(! pub) {
+    err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+    return;
+  }
+
+  // check for private key
+  if(! prv) {
+    err = new Error('forbidden: missing private key');
+    err.status = 403;
+    next(err);
+    return;
+  }
+
+  // validate keys
+  if(! this.keychain.validate(pub, prv)) {
+    err = new Error('forbidden: invalid key');
+    err.status = 401;
+    next(err);
+    return;
+  }
+
+  id = this.keychain.getIdFromPublicKey(pub);
+
+  this.metadata.remove(id, function(err, stream) {
+
+    if(err) {
+      err = new Error('deleting the stream failed');
+      err.status = 500;
+      next(err);
+      return;
+    }
+
+    res.redirect('/');
+
+  });
+
+};
 
