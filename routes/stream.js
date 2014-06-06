a638c09b60f482ed594e6fe789aebb4c18b0879c
Todd Treece <toddtreece@gmail.com>
2014-06-06 10:58:27 -0400

    adds keys and created stream back to create json responder

--- routes/stream.js
+++ routes/stream.js
@@ -166,7 +166,11 @@ exports.create = function(req, res, next) {
         },
         json: function() {
           res.json({
-            success: true
+            success: true,
+            stream: stream,
+            publicKey: self.keychain.publicKey(stream.id),
+            privateKey: self.keychain.privateKey(stream.id),
+            deleteKey: self.keychain.deleteKey(stream.id)
           });
         }
       });
